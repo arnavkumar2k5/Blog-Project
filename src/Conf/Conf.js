ff7380bda358c3f0e32357106333fa8cf6b78d3f
Arnav Kumar <arnavkumar2k5@gmail.com>
2024-08-30 00:08:16 +0530

    tiny mcs solve

--- src/Conf/Conf.js
+++ src/Conf/Conf.js
@@ -4,6 +4,7 @@ const Conf = {
     appwriteDatabaseID : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionID : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketID : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinymceApiKey : String(import.meta.env.VITE_TINY_MCE_API_KEY),
 }
 
 export default Conf
